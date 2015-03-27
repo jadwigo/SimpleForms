e253fddc1b7fe6318174082295eae4d27bc47313
Bob den Otter <bobdenotter@gmail.com>
2015-03-27 15:15:01 +0100

    Update Extension.php

--- Extension.php
+++ Extension.php
@@ -37,7 +37,7 @@ class Extension extends \Bolt\BaseExtension
         return true;
     }
     
-   /**
+    /**
      * Let Bolt know this extension sends e-mails. The user will see a
      * notification on the dashboard if mail is not set up correctly.
      */
